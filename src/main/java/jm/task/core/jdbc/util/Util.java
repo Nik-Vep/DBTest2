d566da87cf8501ad401268a35c94e31d6ea7aab2
nik_vep <nikitka1992.1992@mail.ru>
2024-10-22 15:03:20 +0300

    .

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,44 +7,13 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private final static String USER_NAME = "root";
-    private final static String PASSWORD = "root";
-    private static volatile Connection connection;
+
     private static volatile SessionFactory sessionFactory;
 
     private Util() {
-    }
-
-    public static Connection getConnection() {
-        Connection localConnection = connection;
-        try {
-            if (localConnection == null || localConnection.isClosed()) {
-                synchronized (Util.class) {
-                    localConnection = connection;
-                    if (localConnection == null || localConnection.isClosed()) {
-                        try {
-                            Driver driver = new com.mysql.cj.jdbc.Driver();
-                            DriverManager.registerDriver(driver);
-                            localConnection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
-                            connection = localConnection;
-                        } catch (SQLException e) {
-                            System.out.println("Connection failed: " + e.getMessage());
-                        }
-                    }
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println("Failed to check connection state: " + e.getMessage());
-        }
-        return localConnection;
     }
 
     public static SessionFactory getSessionFactory() {
